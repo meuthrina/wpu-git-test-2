fbd1b175f1b7c487cd9513b6d69fb030d7e08eb2
wolsim <annlzghny@gmail.com>
2024-02-20 21:52:37 +0700

    menambahkan paragfraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio natus sapiente ullam! Deleniti tempore dicta officiis temporibus optio porro laudantium perferendis rem quas, assumenda at molestias, repellat quia sapiente. Suscipit.</p>
 </body>
 </html>
